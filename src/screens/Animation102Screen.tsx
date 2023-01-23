8128551ef508183b0de9edaa30b0b19602e90b89
gerashdo <gerardo.jimenezarguelles@gmail.com>
2023-01-23 14:52:36 -0600

    Finish animated screen

--- src/screens/Animation102Screen.tsx
+++ src/screens/Animation102Screen.tsx
@@ -1,13 +1,36 @@
 
 import React from 'react'
-import { StyleSheet } from 'react-native'
+import { Animated, PanResponder, StyleSheet } from 'react-native'
 import { View } from 'react-native'
+import { useRef } from 'react';
 
 export const Animation102Screen = () => {
+
+  const pan = useRef( new Animated.ValueXY() ).current;
+
+  const panResponder = PanResponder.create({
+    onStartShouldSetPanResponder: () => true,
+    onPanResponderMove: Animated.event([
+      null,
+      {
+        dx: pan.x, // x,y are Animated.Value
+        dy: pan.y,
+      },
+    ], { useNativeDriver: false }),
+    onPanResponderRelease: () => {
+      Animated.spring(
+        pan, // Auto-multiplexed
+        {toValue: {x: 0, y: 0}, useNativeDriver: false }, // Back to zero
+      ).start();
+    },
+  });
+
+
   return (
     <View style={ styles.container }>
-        <View 
-            style={ styles.box }
+        <Animated.View
+            { ...panResponder.panHandlers }
+            style={[ pan.getLayout(), styles.box, ]}
         />
     </View>
   )
