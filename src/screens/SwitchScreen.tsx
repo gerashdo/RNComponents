8a1adfa2ef9093f164de483b8e23ecf87139bbfe
gerashdo <gerardo.jimenezarguelles@gmail.com>
2023-01-23 16:42:44 -0600

    Finish switches

--- src/screens/SwitchScreen.tsx
+++ src/screens/SwitchScreen.tsx
@@ -1,24 +1,67 @@
 
 import React, { useState } from 'react'
-import { Switch, View } from 'react-native'
+import { StyleSheet } from 'react-native';
+import { Switch, Text, View } from 'react-native'
+import { CustomSwitch } from '../components/CustomSwitch';
 import { HeaderTitle } from '../components/HeaderTitle';
 import { styles } from '../theme/appTheme';
 
 export const SwitchScreen = () => {
 
-    const [isEnabled, setIsEnabled] = useState(false);
-    const toggleSwitch = () => setIsEnabled(previousState => !previousState);
+    const [ state, setState ] = useState({
+        isActive: false,
+        isHungry: true,
+        isHappy: true,
+    })
+
+    const onChange = ( value: boolean, field: string ) => {
+        setState({
+            ...state,
+            [ field ]: value
+        })
+    }
 
   return (
     <View style={{ flex: 1, ...styles.globalMargin }}>
         <HeaderTitle title='Switches' />
-        <Switch
-            trackColor={{ false: '#767577', true: '#39D481' }}
-            thumbColor='#f4f3f4'
-            // ios_backgroundColor="#3e3e3e"
-            onValueChange={toggleSwitch}
-            value={isEnabled}
-      />
+
+        <View style={ localStyles.switchContainer }>
+            <Text style={{ fontSize: 18 }}>Is active</Text>
+            <CustomSwitch 
+                isOn={ state.isActive }
+                onChange={ ( value ) => onChange( value, 'isActive')}
+            />
+        </View>
+        <View style={ localStyles.switchContainer }>
+            <Text style={{ fontSize: 18 }}>Is Hungry</Text>
+            <CustomSwitch 
+                isOn={ state.isHungry }
+                onChange={ ( value ) => onChange( value, 'isHungry')}
+            />
+        </View>
+        <View style={ localStyles.switchContainer }>
+            <Text style={{ fontSize: 18 }}>Is happy</Text>
+            <CustomSwitch 
+                isOn={ state.isHappy }
+                onChange={ ( value ) => onChange( value, 'isHappy')}
+            />
+        </View>
+
+        <Text style={ localStyles.stateText }>
+            { JSON.stringify( state, null, 4 )}
+        </Text>
     </View>
   )
 }
+
+const localStyles = StyleSheet.create({
+    stateText: {
+        fontSize: 20
+    },
+    switchContainer:{
+        flexDirection: 'row', 
+        justifyContent: 'space-between', 
+        alignItems: 'center',
+        marginBottom: 10,
+    }
+})
